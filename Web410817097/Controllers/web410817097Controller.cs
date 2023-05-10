8d55528c9b5ef37d07e9b1ef75bb6ab5d79d6d9c
王功勳 <xun531@gmail.com>
2023-05-10 20:27:21 +0800

    HW3

---
+++ Web410817097/Controllers/web410817097Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410817097.Controllers
+{
+    public class web410817097Controller : Controller
+    {
+        // GET: web410817097
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
